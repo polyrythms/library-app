d07a4cd32d934e9f2429bef6afe175382e698665
polyrytms <kvasnicyn@gmail.com>
2023-08-18 13:17:33 +0500

    Using old mvc app

---
+++ src/main/java/ru/denis/LibraryProject/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,57 @@
+package ru.denis.LibraryProject.config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.EnumSet;
+
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    //на старте спринг приложения выполняем наш приватный метод registerHiddenFilter()
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerCharacterEncodingFilter(aServletContext);
+        registerHiddenFilter(aServletContext);
+    }
+
+    //метод фильтр, который каждый выходящий http запрос будет считывать и смотреть поле name="_method" value="PATCH"
+    private void registerHiddenFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                //добавляем фильтр HiddenHttpMethodFilter уже реализованный в спринге
+//                "/*" -- фильтр срабатывает для всех адресов
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null, true, "/*");
+    }
+
+    private void registerCharacterEncodingFilter(ServletContext aContext) {
+        EnumSet<DispatcherType> dispatcherTypes = EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD);
+
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+
+        FilterRegistration.Dynamic characterEncoding = aContext.addFilter("characterEncoding", characterEncodingFilter);
+        characterEncoding.addMappingForUrlPatterns(dispatcherTypes, true, "/*");
+    }
+
+}
